2be244fffce6c1b43c08c54624ef63c710edb4c6
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-09-11 14:17:22 +0530

    16

--- src/app/components/dash-board/place-order/place-order.component.ts
+++ src/app/components/dash-board/place-order/place-order.component.ts
@@ -13,10 +13,21 @@ export class PlaceOrderComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadAllCustomers();
+
+    this.allItems.push({"code":"ITL-2585","description":"Sample Product 1","unitPrice":25,"qtyOnHand":245});
+    this.allItems.push({"code":"ITL-2586","description":"Sample Product 2","unitPrice":75,"qtyOnHand":745});
+    this.allItems.push({"code":"ITL-2587","description":"Sample Product 3","unitPrice":84,"qtyOnHand":784});
+    this.allItems.push({"code":"ITL-2588","description":"Sample Product 4","unitPrice":72,"qtyOnHand":965});
+    this.allItems.push({"code":"ITL-2589","description":"Sample Product 5","unitPrice":76,"qtyOnHand":743});
+
+    this.selectedItem=this.allItems[0];
   }
 
   allCustomer: any[] = [];
   selectedCustomer: any=null;
+
+  allItems:any[]=[];
+  selectedItem:any=null;
 
   private loadAllCustomers() {
     this._customerService.getAllCustomers().subscribe(response => {
@@ -30,9 +41,18 @@ export class PlaceOrderComponent implements OnInit {
   }
 
   setCustomer(id: string) {
+
     for (const temp of this.allCustomer){
       if (temp.id===id){
         this.selectedCustomer = temp;
+      }
+    }
+  }
+
+  setItem(id: string) {
+    for (const temp of this.allItems){
+      if (temp.code===id){
+        this.selectedItem = temp;
       }
     }
   }
